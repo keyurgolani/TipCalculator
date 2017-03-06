7f6cdddebabc0e8dc5a86e7c7268d04b30c52d2c
Keyur Golani <keyurrgolani@gmail.com>
2017-03-06 12:18:57 -0800

    Autofocus on text and open keyboard

--- Tip Calculator/ViewController.swift
+++ Tip Calculator/ViewController.swift
@@ -17,6 +17,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        billValueField.becomeFirstResponder()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
